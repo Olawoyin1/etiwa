b91d1cf50f99e19287656f3e5fde84eccc703a13
Olawoyin <yustee2017@gmail.com>
2025-10-07 17:57:06 +0100

    testimohila

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -232,21 +232,11 @@ const SmoothScrollTestimonials: React.FC = () => {
   {
     breakpoint: 1024,
     settings: {
-      slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToShow: 2,
+      slidesToScroll: 2,
       infinite: true,
       dots: true,
       arrows: false,
-    },
-  },
-  {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      initialSlide: 2,
-      arrows: false,
-      centerPadding: "10px", // Add this line to reduce centerPadding for smaller screens
     },
   },
   {
